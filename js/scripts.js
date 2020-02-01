19021fd050e5d36141495459d3e9bcab46e07d62
Rajab Natshah <rajabn@gmail.com>
2020-02-01 11:40:06 +0200

    Issue #3110539: Initialize [Vardoc Theme (Bootstrap 4 - SASS)] theme with Vartheme BS4 (Bootstrap 4 SASS) as the base theme

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Behaviors for the vardoc_theme theme.
+ */
+
+(function ($, _, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.vardoc_theme = {
+    attach: function (context) {
+
+      // vardoc_theme JavaScript behaviors goes here.
+    }
+  };
+
+})(window.jQuery, window._, window.Drupal, window.drupalSettings);
